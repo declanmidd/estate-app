b32274f04c42b64ead873f9316aa48aea2a2233d
Declan Middleton <declanredd1722@gmail.com>
2023-11-29 16:45:45 +1300

    Update search.jsx

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -5,17 +5,14 @@ import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 
 import Property from '../../components/Property';
-import SearchFilters from '../components/SearchFilters';
 import { parseUrl, fetchApiList } from '../../utils/fetchApi';
 
 const Search = ({ properties }) => {
-    const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
     return (
         <Box>
             <Flex
-                onClick={() => setSearchFilters(!searchFilters)}
                 cursor='pointer'
                 bg='gray.100'
                 borderBottom='1px'
@@ -29,7 +26,6 @@ const Search = ({ properties }) => {
                 <Text>Search Property By Filters</Text>
                 <Icon paddingLeft='2' w='7' as={BsFilter} />
             </Flex>
-            {searchFilters && <SearchFilters />}
             <Text fontSize='2xl' p='4' fontWeight='bold'>
                 Properties {router.query.purpose}
             </Text>
@@ -45,26 +41,5 @@ const Search = ({ properties }) => {
         </Box>
     );
 };
-
-export async function getServerSideProps({ query }) {
-    const purpose = query.purpose || 'for-rent';
-    const rentFrequency = query.rentFrequency || 'yearly';
-    const minPrice = query.minPrice || '0';
-    const maxPrice = query.maxPrice || '1000000';
-    const roomsMin = query.roomsMin || '0';
-    const bathsMin = query.bathsMin || '0';
-    const sort = query.sort || 'price-desc';
-    const areaMax = query.areaMax || '35000';
-    const locationExternalIDs = query.locationExternalIDs || '5002';
-    const categoryExternalID = query.categoryExternalID || '4';
-
-    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
-
-    return {
-        props: {
-            properties: data?.hits,
-        },
-    };
-}
 
 export default Search;
